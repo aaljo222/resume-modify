5e1ebba4ee69c630dd44dd400f19bdd00af7a972
aaljo222 <125717229+aaljo222@users.noreply.github.com>
2025-08-28 15:03:33 +0900

    Update Data.js
    노코드 6기 이력 추가

--- src/Schemas/Data.js
+++ src/Schemas/Data.js
@@ -221,6 +221,14 @@ export const Data = {
           "JAVA , 스프링,JAVASCRIPT,리액트 ,HTML,CSS,SQL, JPA , 파이선"
         ],
       },
+       {
+        name: "프로젝트기반 웹&앱(자바,스프링,리액트,노코드) SW개발자 양성과정",
+        company: "미금 그린아카데미 노코드 6기",
+        period: "2025년 7월 15일 -  2026년 2월 10일(현재 진행중)",
+        description: [
+          "JAVA , 스프링부트 ,JAVASCRIPT,리액트 ,HTML,CSS,SQL, JPA"
+        ],
+      },
     ],
   },
 };

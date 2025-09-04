9bb1d99893750565eadcd0d9c276bbfee40431e7
aaljo222 <aaljo2@gmail.com>
2025-09-04 18:24:46 +0900

    대대적인 수정9

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        // 이 키 이름이 클래스 이름이 됩니다: shadow-soft
+        soft: "0 10px 30px rgba(2, 6, 23, 0.08)",
+      },
+    },
   },
   plugins: [],
 };
